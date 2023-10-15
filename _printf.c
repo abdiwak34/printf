8ff015615377fb0b6f3e679ff3960fd62aaff785
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-10-15 14:33:22 -0700

    another change

--- _printf.c
+++ _printf.c
@@ -1,54 +1,56 @@
 #include <stdio.h>
-#include "main.h"
-#include <string.h>
 #include <stdarg.h>
-/**
- *_printf - is function that produces output according to a format
- *@format: is argument that out function
- *Return: always lenght of string
- */
+#include "main.h"
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+    va_list args;
+    va_start(args, format);
 
-	int count = 0;
-	const char *c;
-	for( c = format; *c != '\0'; c++)
-	{
-		if(*c == '\n')
-		{
-			c++;
-		switch(*c)
-		{
-			case 'c':
-				int ch = va_arg(args, int);
-				_putchar(ch);
-				count++;
-				break;
-		}
-			case 's':
-		{
-			const char *str = va_arg(args, const char *);
-			while (*str != '\n')
-			{
-				_putchar(*str);
-				str++;
-			}
-			break;
-		}
-			default:
-		{
-			break;
-		}
-		}
-		else
-		{
-			_putchar(*c);
-			count++;
-		}
-		}
-	va_end(args);
-	return count;
+    int count = 0;
+    const char *c;
+
+    for (c = format; *c != '\0'; c++) {
+        if (*c == '%') {
+            c++; // Move past the '%'
+
+            // Handle conversion specifiers
+            switch (*c) {
+                case 'c': {
+                    // Print a character
+                    int ch = va_arg(args, int);
+                    putchar(ch);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    // Print a string
+                    const char *str = va_arg(args, const char *);
+                    while (*str != '\0') {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    // Print a '%'
+                    putchar('%');
+                    count++;
+                    break;
+                }
+                default:
+                    // Invalid conversion specifier, ignore
+                    break;
+            }
+        } else {
+            // Regular character, print it
+            putchar(*c);
+            count++;
+        }
+    }
+
+    va_end(args);
+
+    return count;
 }
