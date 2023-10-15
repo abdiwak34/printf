a9598153807dae62410129de7a85f05d2f0c5029
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-10-15 15:51:33 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,13 +4,12 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	char c;
+    int count = 0;
+    char c;
     va_list args;
     va_start(args, format);
 
-
-    while ((c = *format++) != '\0')
+    while ((c = *format++))
     {
         if (c != '%')
         {
@@ -20,38 +19,31 @@ int _printf(const char *format, ...)
         else
         {
             c = *format++;
-            switch (c)
+            if (c == 'c')
             {
-                case 'c':
-                    {
-                        char ch = (char)va_arg(args, int);
-                        putchar(ch);
-                        count++;
-                        break;
-                    }
-                case 's':
-                    {
-                        const char *str = va_arg(args, const char *);
-                        while (*str != '\0')
-                        {
-                            putchar(*str++);
-                            count++;
-                        }
-                        break;
-                    }
-                case '%':
-                    {
-                        putchar('%');
-                        count++;
-                        break;
-                    }
-                default:
-                    {
-                        putchar('%');
-                        putchar(c);
-                        count += 2;
-                        break;
-                    }
+                char ch = (char)va_arg(args, int);
+                putchar(ch);
+                count++;
+            }
+            else if (c == 's')
+            {
+                const char *str = va_arg(args, const char *);
+                while (*str)
+                {
+                    putchar(*str++);
+                    count++;
+                }
+            }
+            else if (c == '%')
+            {
+                putchar('%');
+                count++;
+            }
+            else
+            {
+                putchar('%');
+                putchar(c);
+                count += 2;
             }
         }
     }
