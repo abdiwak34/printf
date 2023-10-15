96dd9d56ebc01ed3b7847d217d73b2a3f3ab28d8
bereket-kume <bereketkume@gmail.com>
2023-10-15 01:20:34 -0700

    bereket

--- _printf.c
+++ _printf.c
@@ -13,8 +13,36 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count;
+	char ch;
+
+	count = 0;
 	va_start(args, format);
-	count = strlen(format);
+	while((ch = *format++) != '\n')
+	{
+	if(ch == '%')
+	{
+	switch(ch)
+	{
+		case 'c':
+			int cha;
+			cha = va_arg(args, int);
+			_putchar(cha);
+			break;
+		case 's':
+			const char* value;
+			value = va_arg(args, const char*);
+			while(value != '\n')
+			{
+				_putchar(value);
+				count++;
+			}
+			break;
+		case '%':
+			_putchar('%');
+			break;
+
+	}
+
        	va_end(args);
 	return count;
 }
