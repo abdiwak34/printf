2b00893ce8ac8606b40ebe8f0595d0b9fc270475
bereket-kume <bereketkume@gmail.com>
2023-10-15 16:01:20 -0700

    done

--- _printf.c
+++ _printf.c
@@ -4,50 +4,53 @@
 
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    char c;
-    va_list args;
-    va_start(args, format);
+	int count = 0;
+	char c;
+	va_list args;
 
-    while ((c = *format++))
-    {
-        if (c != '%')
-        {
-            putchar(c);
-            count++;
-        }
-        else
-        {
-            c = *format++;
-            if (c == 'c')
-            {
-                char ch = (char)va_arg(args, int);
-                putchar(ch);
-                count++;
-            }
-            else if (c == 's')
-            {
-                const char *str = va_arg(args, const char *);
-                while (*str)
-                {
-                    putchar(*str++);
-                    count++;
-                }
-            }
-            else if (c == '%')
-            {
-                putchar('%');
-                count++;
-            }
-            else
-            {
-                putchar('%');
-                putchar(c);
-                count += 2;
-            }
-        }
-    }
+	va_start(args, format);
 
-    va_end(args);
-    return count;
+	while ((c = *format++))
+	{
+	if (c != '%')
+	{
+		putchar(c);
+		count++;
+	}
+	else
+	{
+		c = *format++;
+		if (c == 'c')
+	{
+		char ch = (char)va_arg(args, int);
+
+		putchar(ch);
+		count++;
+	}
+	else if (c == 's')
+	{
+	const char *str = va_arg(args, const char *);
+
+		while (*str)
+		{
+			putchar(*str++);
+			count++;
+		}
+	}
+	else if (c == '%')
+	{
+	putchar('%');
+	count++;
+	}
+	else
+	{
+	putchar('%');
+	putchar(c);
+	count += 2;
+	}
+	}
+	}
+
+	va_end(args);
+	return (count);
 }
