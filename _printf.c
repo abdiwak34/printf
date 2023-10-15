fce61de23d06eff93846554e7d97d77d947318a3
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-10-15 14:59:22 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,58 +1,69 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
+
+int (*find_function(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_dec},
+		{"r", print_rev},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+
+	while (find_f[i].sc)
+	{
+		if (find_f[i].sc[0] == (*format))
+			return (find_f[i].f);
+		i++;
+	}
+	return (NULL);
+}
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count;
-    const char *c;
-    va_start(args, format);
-    count = 0;
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
 
-    for (c = format; *c != '\0'; c++) {
-        if (*c == '%') {
-            c++;
-            switch (*c) {
-                case 'c': {
-                    int ch = va_arg(args, int);
-                    putchar(ch);
-                    count++;
-                    break;
-                }
-                case 's': 
-			  {
-                    const char *str = va_arg(args, const char *);
-                    while (*str != '\0') {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                    break;
-                }
-		case 'd':
-		case 'i':
-			  {
-				  int num = va_arg(args, int);
-				  _putchar(num);
-				  count++;
-				  break;
-			  }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    break;
-            }
-        } else {
-            putchar(*c);
-            count++;
-        }
-    }
-
-    va_end(args);
-
-    return count;
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			cprint++;
+			i++;
+		}
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
+		{
+			cprint += f(ap);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		cprint++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(ap);
+	return (cprint);
 }
