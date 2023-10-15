f1f116ccb7661bb8a9dc31463ae3ed7e6c844063
bereket-kume <bereketkume@gmail.com>
2023-10-15 12:41:17 -0700

    done

--- _printf.c
+++ _printf.c
@@ -16,7 +16,6 @@ int _printf(const char *format, ...)
 
 	count = 0;
 	va_start(args, format);
-<<<<<<< HEAD
 	while(*format++ != '\n')
 	{
 	if((ch = *format) != '%')
@@ -24,11 +23,10 @@ int _printf(const char *format, ...)
 		ch++;
 	}
 	switch(*++format)
-=======
+	{
 	while ((ch = *format++) != '\0')
 	{
 	if (ch == '%') 
->>>>>>> d6d4670154d27da65ced530b544cd2d8b00620e5
 	{
 		char cha;
 		const char* value;
@@ -66,13 +64,8 @@ int _printf(const char *format, ...)
 	count++;
 	}
 	}
-	va_end(args);
-	return count;
 }
-<<<<<<< HEAD
 }
-
-
-
-=======
->>>>>>> d6d4670154d27da65ced530b544cd2d8b00620e5
+va_end(args);
+return count;
+}
