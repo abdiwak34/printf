b05820f971dcc1d62f8ffc1aedab1910125023f0
bereket-kume <bereketkume@gmail.com>
2023-10-15 14:45:35 -0700

    done

--- _printf.c
+++ _printf.c
@@ -5,26 +5,22 @@
 int _printf(const char *format, ...)
 {
     va_list args;
-    va_start(args, format);
-
-    int count = 0;
+    int count;
     const char *c;
+    va_start(args, format);
+    count = 0;
 
     for (c = format; *c != '\0'; c++) {
         if (*c == '%') {
-            c++; // Move past the '%'
-
-            // Handle conversion specifiers
+            c++;
             switch (*c) {
                 case 'c': {
-                    // Print a character
                     int ch = va_arg(args, int);
                     putchar(ch);
                     count++;
                     break;
                 }
                 case 's': {
-                    // Print a string
                     const char *str = va_arg(args, const char *);
                     while (*str != '\0') {
                         putchar(*str);
@@ -34,17 +30,14 @@ int _printf(const char *format, ...)
                     break;
                 }
                 case '%': {
-                    // Print a '%'
                     putchar('%');
                     count++;
                     break;
                 }
                 default:
-                    // Invalid conversion specifier, ignore
                     break;
             }
         } else {
-            // Regular character, print it
             putchar(*c);
             count++;
         }
