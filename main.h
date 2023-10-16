b28e4cef9d13827392ef5208ad67c2f6752f72aa
abdiwak34 <140180098+abdiwak34@users.noreply.github.com>
2023-10-16 13:11:10 -0700

    Update main.h

--- main.h
+++ main.h
@@ -2,11 +2,4 @@
 #define _MAIN_
 int _printf(const char *format, ...);
 int _putchar(char);
-int printf_char(va_list arr);
-int printf_string(va_list s);
-typedef struct formt
-{
-	char ch;
-	int (*func)(va_list);
-} frmt
 #endif
