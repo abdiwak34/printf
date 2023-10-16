e58ef5ff77bdd27af63d4bdfa508f328b104d16b
abdiwak34 <140180098+abdiwak34@users.noreply.github.com>
2023-10-16 13:07:06 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,35 +10,45 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while (c = *format++)
+	while ((c = *format++))
 	{
-		if (c != '%')
+	if (c != '%')
+	{
+		putchar(c);
+		count++;
+	}
+	else
+	{
+		c = *format++;
+		if (c == 'c')
+	{
+		char ch = (char)va_arg(args, int);
+
+		putchar(ch);
+		count++;
+	}
+	else if (c == 's')
+	{
+	const char *str = va_arg(args, const char *);
+
+		while (*str)
 		{
-			putchar(c);
+			putchar(*str++);
 			count++;
 		}
-		else
-		{
-			c = *format++;
-			if (c == 'c' || c == 's' || c == '%')
-			{
-				char ch = (char)va_arg(args, int);
-				switch(ch)
-				{
-					case 'c':
-						printf_char(args);
-					case 's':
-						printf_string(args);
-					case '%':
-						putchar('%');
-					default:
-						putchar('%');
-						putchar(c);
-						count += 2;
-				}
-
-			}
-		}
+	}
+	else if (c == '%')
+	{
+	putchar('%');
+	count++;
+	}
+	else
+	{
+	putchar('%');
+	putchar(c);
+	count += 2;
+	}
+	}
 	}
 
 	va_end(args);
