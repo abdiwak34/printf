ab2104494871eb7d75ee0a8e6c616d12054383a8
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-10-17 04:25:21 -0700

    done

--- _printf.c
+++ _printf.c
@@ -10,12 +10,11 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int num;
-
+	int count;
 	va_start(args, format);
-	num = custom_print(format, args);
+	count = custom_print(format, args);
 	va_end(args);
-	return (num);
+	return (count);
 }
 /**
  *custom_print - is function that check input type
@@ -25,7 +24,46 @@ int _printf(const char *format, ...)
  */
 int custom_print(const char *format, va_list args)
 {
-	int count;
-        count = vprintf(format, args);
+	int count = 0;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int c;
+				c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					count++;
+					str++;
+				}
+			}
+			else if ((*format == 'd' || *format == 'i'))
+			{
+				int num = va_arg(args, int);
+				_putchar(num);
+				printf("%",num);
+				count++;
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+		}
+	}
 	return (count);
 }
