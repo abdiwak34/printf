eb605d73eb6b7c0e6a99de79a3ca6b510dbfbe4a
bereket-kume <bereketkume@gmail.com>
2023-10-17 01:39:56 -0700

    done

--- _printf.c
+++ _printf.c
@@ -2,14 +2,18 @@
 #include "main.h"
 #include <string.h>
 #include <stdarg.h>
+/**
+ *_printf - is function that return lenght
+ *@format: is first parameter
+ *Return: always count
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count;
+
 	va_start(args, format);
 	count = vprintf(format, args);
-
 	va_end(args);
-
 	return (count);
 }
