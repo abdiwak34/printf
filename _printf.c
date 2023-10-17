53bb7d65a54e8b3a2819cd49d249f56ebb5a37f9
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-10-17 02:16:20 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -17,3 +17,42 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+
+int custom_print(const char* format,va_list args)
+{
+	int count = 0;
+	while(*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int c;
+				c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char* str = va_arg(args, char*);
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					count++;
+					str++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+		}
+	}
+	return (count);
+}
