60a9ce9a732d09964d7c9629df6cb2014303f01c
bereket-kume <bereketkume@gmail.com>
2023-10-17 02:32:47 -0700

    done

--- _printf.c
+++ _printf.c
@@ -13,15 +13,21 @@ int _printf(const char *format, ...)
 	int count;
 
 	va_start(args, format);
-	count = custom_print(format,args);
+	count = custom_print(format, args);
 	va_end(args);
 	return (count);
 }
-
-int custom_print(const char* format,va_list args)
+/**
+ *custom_print - is function that check input type
+ *@format: our first parameter
+ *@args: our second parameter
+ *Return: count to _printf function
+ */
+int custom_print(const char *format, va_list args)
 {
 	int count = 0;
-	while(*format != '\0')
+
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
@@ -29,13 +35,15 @@ int custom_print(const char* format,va_list args)
 			if (*format == 'c')
 			{
 				int c;
+
 				c = va_arg(args, int);
 				_putchar(c);
 				count++;
 			}
 			else if (*format == 's')
 			{
-				char* str = va_arg(args, char*);
+				char *str = va_arg(args, char*);
+
 				while (*str != '\0')
 				{
 					_putchar(*str);
