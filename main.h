3b4b3bfe03f3331a82e96f6adcdc52f1871b796d
bereket-kume <bereketkume@gmail.com>
2023-10-16 23:03:55 -0700

    done

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef _MAIN_
 #define _MAIN_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int _printf(const char *format, ...);
 int _putchar(char);
 #endif
